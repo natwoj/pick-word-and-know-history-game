870caa2cc0615df6279313c281bf7dc2645e0ee5
_natwoj <info@nwojcik.com>
2021-07-16 22:35:46 +0200

    start with js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var buttonChange = document.getElementById("buttonChange");
+buttonChange.addEventListener('click', changeThis);
+
+function () {
+var historyDiv = getElementById("history");
+var adjective = "<span class='change'>" + getElementById("adjective").value + "</span>";
+
+
+
+
+historyDiv.innerHTML = history;
+}
+
