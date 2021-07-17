37e89f1798dfd2dd2bfa630bb79cc80755895672
_natwoj <info@nwojcik.com>
2021-07-17 21:55:29 +0200

    adding few vars

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 var buttonChange = document.getElementById("buttonChange");
 buttonChange.addEventListener('click', changeThis);
 
-function () {
-var historyDiv = getElementById("history");
-var adjective = "<span class='change'>" + getElementById("adjective").value + "</span>";
+function changeThis() {
+var historyDiv = document.getElementById("history");
+var adjective = "<span class='change'>" + document.getElementById("adjective").value + "</span>";
+/*here to add more variable for other inputs*/
 
 
 
+var history = "<h1> DANCE STORY </h1>";
+history += "This " + adjective + " day";
+/* rest of history*/
 
 historyDiv.innerHTML = history;
 }
