d52e67719f82a6823ff9536190956379bbade89f
_natwoj <info@nwojcik.com>
2021-07-19 22:38:35 +0200

    spread var history

--- javascript.js
+++ javascript.js
@@ -4,13 +4,12 @@ buttonChange.addEventListener('click', changeThis);
 function changeThis() {
 var historyDiv = document.getElementById("history");
 var adjective = "<span class='change'>" + document.getElementById("adjective").value + "</span>";
-/*here to add more variable for other inputs*/
-
+var verb = "<span class='change'>" + document.getElementById("verb").value + "</span>";
+var roomOne = "<span class='change'>" + document.getElementById("roomOne").value + "</span>";
 
 
 var history = "<h1> DANCE STORY </h1>";
-history += "This " + adjective + " day";
-/* rest of history*/
+history += "This " + adjective + " day " + " Mark " + verb + " in " + roomOne   
 
 historyDiv.innerHTML = history;
 }
