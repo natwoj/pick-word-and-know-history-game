ce79af1bdb1b9242e3878163359e35ffaeeed470
_natwoj <info@nwojcik.com>
2021-07-20 21:36:16 +0200

    bug fixing

--- javascript.js
+++ javascript.js
@@ -2,17 +2,21 @@ var buttonChange = document.getElementById("buttonChange");
 buttonChange.addEventListener('click', changeThis);
 
 function changeThis() {
-var historyDiv = document.getElementById("history");
+var historyDiv= document.getElementById("history");
 var adjective = "<span class='change'>" + document.getElementById("adjective").value + "</span>";
 var verb = "<span class='change'>" + document.getElementById("verb").value + "</span>";
 var roomOne = "<span class='change'>" + document.getElementById("roomOne").value + "</span>";
 var adjectiveTwo = "<span class='change'>" + document.getElementById("adjectiveTwo").value + "</span>";
 var verbTwo = "<span class='change'>" + document.getElementById("VerbTwo").value + "</span>";
 
-var history = "<h1> DANCE STORY </h1>";
-history += "This " + adjective + " day " + " Mark " + verb + " in " + roomOne "."; 
-history += "Meanwhile" + adjectiveTwo + " lady " verbTwo " him a favor";
+var history = "<h1> DANCE STORY </h1>:"; 
+history += "This " + adjective + " day ";
+/*
+history += "Mark " + verb + " into/on ";
 
++ roomOne + ".";
+history += "Mark's cousin " + adjectiveTwo + "everybody";
+history += "Elena" + verbTwo + "outside house";
+*/
 historyDiv.innerHTML = history;
 }
-
